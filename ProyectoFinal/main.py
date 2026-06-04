716a6c583282677a75d06526dc0a1427b76fab86
SanB360 <santiagobauza05@gmail.com>
2026-06-04 20:22:34 -0300

    menu base

--- ProyectoFinal/main.py
+++ ProyectoFinal/main.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+def menu():
+    print("="*50)
+    print("Bienvenido al sistema de gestión de Puertos")
+    print("1. Ver/seleccionar dispositivo a analizar")
+    print("2. Editar puertos")
+    print("3. Detectar vulnerabilidades")
+    print("4. Salir")
+    print("="*50)
+
+menu()
