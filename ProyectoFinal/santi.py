3865b26fff67b2162ed85278da267897d522f52d
SanB360 <santiagobauza05@gmail.com>
2026-06-09 23:26:59 -0300

    mejoras

--- ProyectoFinal/santi.py
+++ ProyectoFinal/santi.py
@@ -1,20 +1,19 @@
 import nmap
 nm = nmap.PortScanner()
 
-host = input("Ingresá la IP a escanear: ")
-puerto = input("Ingresá el puerto a escanear: ")
-
-nm.scan(host, puerto)
+nm.scan('192.168.56.1', arguments='-A -sV --open -T4')
 
 for host in nm.all_hosts():
     print(f"Nmap scan report for {host}")
     print(f"Host is {nm[host].state()}")
     print()
-    print(f"{'PORT':<10} {'STATE':<10} {'SERVICE'}")
-    print("-" * 30)
+    print(f"{'PORT':<10} {'STATE':<10} {'SERVICE':<15} {'PRODUCT':<20} {'VERSION'}")
+    print("-" * 70)
     
     for proto in nm[host].all_protocols():
         for puerto in nm[host][proto].keys():
             estado = nm[host][proto][puerto]['state']
             nombre = nm[host][proto][puerto]['name']
-            print(f"{str(puerto)+'/'+proto:<10} {estado:<10} {nombre}")
+            producto = nm[host][proto][puerto]['product']
+            version = nm[host][proto][puerto]['version']
+            print(f"{str(puerto)+'/'+proto:<10} {estado:<10} {nombre:<15} {producto} {version}")
